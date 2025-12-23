cb8d68dcfddc75c0ccf68f1660adb83876acf1d3
silasenapong <sila_sanapong@cmu.ac.th>
2025-12-23 16:14:50 +0700

    Lab10

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,18 +1,31 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
+int main()
+{
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
-    source.close();
-    dest.close();
+	// กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ
+
+	string text;
+
+	dest << "-------------------- BOOM ---------------------\n";
+
+	while (getline(source, text))
+	{
+		dest << text << endl;
+	}
+
+	dest << "-------------------- HA!! ---------------------";
+
+	// โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+
+	source.close();
+	dest.close();
 	return 0;
 }
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,47 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    string num;
+    float sum = 0.0, mean, num2 = 0.0, standard_deviation;
+    int n = 0;
+
+    ifstream source;
+    ifstream source2;
+    source.open("score.txt");
+    source2.open("score.txt");
+
+    while (getline(source, num))
+    {
+        n++;
+        sum += stof(num);
+
+        // cout << n << "\t" << sum << endl;
+    }
+
+    mean = sum / n;
+
+    while (getline(source2, num))
+    {
+        num2 += pow(stof(num) - mean, 2);
+        // cout << num2 << endl;
+    }
+
+    // cout << n << " " << mean << " " << num2 << endl;
+
+    standard_deviation = sqrt((1.0 / n) * num2);
+
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standard_deviation << endl;
+
+    source.close();
+    source2.close();
+}
